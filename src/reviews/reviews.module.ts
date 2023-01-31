de31d41a24d74a03a593cdc1d5af511148539c1d
Pavel <pav.korotkov.03@gmail.com>
2023-01-31 11:26:29 +0300

    fix: after eslint refactor

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -3,9 +3,10 @@ import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Reviews } from './reviews.entity';
+import { ImagesModule } from './../images/images.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Reviews])],
+  imports: [TypeOrmModule.forFeature([Reviews]), ImagesModule],
   controllers: [ReviewsController],
   providers: [ReviewsService],
 })
