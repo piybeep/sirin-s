a4de1c822e58abd4c83802bc2c4c124fee194404
Pavel <pav.korotkov.03@gmail.com>
2023-02-02 12:24:54 +0300

    validation

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -1,13 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger/dist';
+import { IsNotEmpty } from 'class-validator/types/decorator/decorators';
 export class CreateCourseDto {
   @ApiProperty()
+  @IsNotEmpty()
   fullname_applicant: string;
+
   @ApiProperty()
+  @IsNotEmpty()
   fullname_student: string;
+
   @ApiProperty()
+  @IsNotEmpty()
   age_student: number;
+
   @ApiProperty()
+  @IsNotEmpty()
   contact: string;
+
   @ApiProperty()
+  @IsNotEmpty()
   place: string;
 }
--- src/crew/dto/create-crew.dto.ts
+++ src/crew/dto/create-crew.dto.ts
@@ -1,20 +1,32 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator/types/decorator/decorators';
 
 export class CreateCrewDto {
   @ApiProperty()
+  @IsNotEmpty()
   fullname: string;
+
   @ApiProperty()
+  @IsNotEmpty()
   vacancy: string;
+
   @ApiPropertyOptional()
   sub_vacancy?: string;
+  
   @ApiProperty()
   photo_id: number;
+
   @ApiProperty()
   education: string;
+
   @ApiProperty()
   experience: string;
+
   @ApiPropertyOptional()
   achievements?: string;
+
   @ApiPropertyOptional()
+  
   images?: [number];
+
 }
