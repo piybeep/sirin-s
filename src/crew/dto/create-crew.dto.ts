480545f6afd5fbc524110a44c8f334b6181be38c
Pavel <pav.korotkov.03@gmail.com>
2023-02-02 16:30:59 +0300

    fix: validation

--- src/crew/dto/create-crew.dto.ts
+++ src/crew/dto/create-crew.dto.ts
@@ -1,34 +1,47 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsArray, IsNotEmpty, IsNumber } from 'class-validator';
+import {
+  IsArray,
+  IsNotEmpty,
+  IsString,
+  IsInt,
+  IsPositive,
+} from 'class-validator';
 
 export class CreateCrewDto {
   @ApiProperty()
   @IsNotEmpty()
+  @IsString()
   fullname: string;
 
   @ApiProperty()
   @IsNotEmpty()
+  @IsString()
   vacancy: string;
 
   @ApiPropertyOptional()
   @IsNotEmpty()
+  @IsString()
   sub_vacancy?: string;
 
   @ApiProperty()
-  @IsNumber()
+  @IsInt()
+  @IsPositive()
   photo_id: number;
 
   @ApiProperty()
   @IsNotEmpty()
+  @IsString()
   education: string;
 
   @ApiProperty()
   @IsNotEmpty()
+  @IsString()
   experience: string;
 
   @ApiPropertyOptional()
   @IsNotEmpty()
+  @IsString()
   achievements?: string;
 
   @ApiPropertyOptional({ isArray: true })
