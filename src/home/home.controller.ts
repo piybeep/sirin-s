bce472f09d988cd9f29e12043a78aa9221dc1956
Daniil <support@danyatochka.ru>
2023-12-12 20:59:09 +0300

    fix: изменил метод изменения Home и добавил guard

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Get, Patch } from '@nestjs/common';
+import { Body, Controller, Get, Put, UseGuards } from '@nestjs/common';
 import { HomeService } from './home.service';
 import { UpdateHomeDto } from './dto/update-home.dto';
+import { AccessTokenGuard } from '../sessions/guards/access-token.guard';
+import { ApiTags } from '@nestjs/swagger/dist/decorators';
 
+@ApiTags('Данные на главной странице')
 @Controller('home')
 export class HomeController {
   constructor(private readonly homeService: HomeService) {}
@@ -11,7 +14,8 @@ export class HomeController {
     return this.homeService.findAll();
   }
 
-  @Patch()
+  @Put()
+  @UseGuards(AccessTokenGuard)
   update(@Body() updateHomeDto: UpdateHomeDto) {
     return this.homeService.update(updateHomeDto);
   }
